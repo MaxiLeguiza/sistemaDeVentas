de24ef1f79078c376823ffc4a4abc66d6960d62c
MaxiLeguiza <leguimanfire17@gmail.com>
2025-05-26 01:01:19 -0300

    Se crea front con Menu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import Clientes from "./components/Clientes";
+import Proveedores from "./components/Proveedores";
+import Categorias from "./components/Categorias";
+import Productos from "./components/Productos";
+import Ventas from "./components/Ventas";
+import React from "react";
+import "./App.css";
+import Menu from "./components/Menu";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Sistemas de Ventas</h1>
+       <Menu />
+
+      {/* Aquí puedes agregar los otros módulos */}
+    </div>
+  );
+}
+
+export default App;
